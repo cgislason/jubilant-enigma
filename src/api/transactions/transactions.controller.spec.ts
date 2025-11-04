b38040c1c693c980eceaf8013a4cb36b122599e8
Colin Gislason <cgislason@gmail.com>
2025-11-03 20:53:40 -0800

    Add basic e2e tests

--- src/api/transactions/transactions.controller.spec.ts
+++ src/api/transactions/transactions.controller.spec.ts
@@ -18,23 +18,4 @@ describe('AccountsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-
-  describe('POST /transactions', () => {
-    const subject = (body) => controller.create(body);
-
-    // afterEach(() => {
-    //   //TODO: __test_transactions__.clearTransactions();
-    // });
-
-    // it('should create a transaction', () => {
-    //   const body = {
-    //     id: crypto.randomUUID(),
-    //     name: 'test account',
-    //     balance: 0,
-    //     direction: 'debit',
-    //   };
-    //   const result = subject(body);
-    //   expect(result).toMatchObject(body);
-    // });
-  });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { __test_accounts__ } from './../src/shared/ledger/accounts/accounts.repo';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,77 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  afterEach(() => {
+    __test_accounts__.clearAccounts();
+  });
+
+  it('/accounts POST', async () => {
+    const server = app.getHttpServer();
+
+    const account = {
+      id: crypto.randomUUID(),
+      direction: 'credit',
+      name: `test-account`,
+      balance: 0,
+    };
+
+    await request(server).post('/accounts').send(account);
+
+    return request(server)
       .get('/accounts')
       .expect(200)
-      .expect('[]');
+      .expect(`[${JSON.stringify(account)}]`);
+  });
+
+  it('/transactions POST', async () => {
+    const server = app.getHttpServer();
+
+    const credit_account = {
+      id: 'credit_account',
+      direction: 'credit',
+      balance: 0,
+      name: 'credit_account',
+    };
+    const debit_account = {
+      id: 'debit_account',
+      direction: 'debit',
+      balance: 0,
+      name: 'debit_account',
+    };
+    const transaction = {
+      id: crypto.randomUUID(),
+      entries: [
+        {
+          direction: 'credit',
+          amount: 100,
+          account_id: credit_account.id,
+        },
+        {
+          direction: 'debit',
+          amount: 100,
+          account_id: debit_account.id,
+        },
+      ],
+    };
+
+    const expectedAccounts = [
+      {
+        ...credit_account,
+        balance: 100,
+      },
+      {
+        ...debit_account,
+        balance: 100,
+      },
+    ];
+
+    await request(server).post('/accounts').send(credit_account);
+    await request(server).post('/accounts').send(debit_account);
+    await request(server).post('/transactions').send(transaction);
+
+    return request(server)
+      .get('/accounts')
+      .expect(200)
+      .expect(JSON.stringify(expectedAccounts));
   });
 });
